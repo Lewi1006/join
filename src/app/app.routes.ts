084797cb5064d76522a70f9f166b47b60c8f8f0f
Lewi1006 <coach.wind11@gmail.com>
2026-08-12 21:29:30 +0200

    feat: help page router link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { ContactsComp } from './components/contacts-comp/contacts-comp';
+import { HelpComp } from './components/help-comp/help-comp';
 
 export const routes: Routes = [
     {
         path: "",
         component: ContactsComp,
+    },
+    {
+        path: 'help',
+        component: HelpComp,
+
     }
 ];
---
+++ src/app/components/help-comp/help-comp.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-help-comp',
+    imports: [],
+    templateUrl: './help-comp.html',
+    styleUrl: './help-comp.scss',
+})
+export class HelpComp {}
