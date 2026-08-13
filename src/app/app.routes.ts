55b773fd159cb2971cefa45653e068b9492a5226
Lewi1006 <coach.wind11@gmail.com>
2026-08-13 10:32:51 +0200

    feat: hover menu overlay

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { ContactsComp } from './components/contacts-comp/contacts-comp';
 import { HelpComp } from './components/help-comp/help-comp';
+import { LoginComp } from './components/login-comp/login-comp';
 
 export const routes: Routes = [
     {
@@ -10,6 +11,9 @@ export const routes: Routes = [
     {
         path: 'help',
         component: HelpComp,
-
-    }
+    },
+    {
+        path: 'logout',
+        component: LoginComp,
+    },
 ];
