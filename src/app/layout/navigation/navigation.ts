99d0b037be0a2cfb8aa678f15a229da57d4a4a7e
Naji Khalifeh <naji.khalifeh@gmail.com>
2026-08-13 12:54:53 +0200

    feat: sidebar and footer added

--- src/app/layout/navigation/navigation.ts
+++ src/app/layout/navigation/navigation.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from '../footer/footer';
+// import { BoardComp } from "../../components/board-comp/board-comp";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
-  imports: [],
+  imports: [Footer, RouterLink, RouterLinkActive],
   templateUrl: './navigation.html',
   styleUrl: './navigation.scss',
 })
