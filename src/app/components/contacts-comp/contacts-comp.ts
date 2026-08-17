b216295d957f96c741326f0676cf4c3bbdaaed49
Naji Khalifeh <naji.khalifeh@gmail.com>
2026-08-17 10:59:33 +0200

    feat:  contact page structure added

--- src/app/components/contacts-comp/contacts-comp.ts
+++ src/app/components/contacts-comp/contacts-comp.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactsCard } from './contacts-card/contacts-card';
+import { ContactsList } from './contacts-list/contacts-list';
 
 @Component({
   selector: 'app-contacts-comp',
-  imports: [],
+  imports: [ContactsCard, ContactsList],
   templateUrl: './contacts-comp.html',
   styleUrl: './contacts-comp.scss',
 })
---
+++ src/app/components/contacts-comp/contacts-list/contacts-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contacts-list',
+    imports: [],
+    templateUrl: './contacts-list.html',
+    styleUrl: './contacts-list.scss',
+})
+export class ContactsList {}
