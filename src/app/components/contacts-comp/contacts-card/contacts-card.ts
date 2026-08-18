2bff79e8e1502d1bcc47f44dd5b7ccca378e3b73
Lewi1006 <coach.wind11@gmail.com>
2026-08-18 10:28:33 +0200

    feat: for loop contacts list

--- src/app/components/contacts-comp/contacts-card/contacts-card.ts
+++ src/app/components/contacts-comp/contacts-card/contacts-card.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-// import { Contact } from '../../../shared/interfaces/contact.interface';
 import { ContactsService } from '../../../shared/services/contacts.service';
 
 @Component({
@@ -9,7 +8,5 @@ import { ContactsService } from '../../../shared/services/contacts.service';
     styleUrl: './contacts-card.scss',
 })
 export class ContactsCard {
-    // @Input()contact!:Contact;
-     dbService = inject(ContactsService);
-
+    dbService = inject(ContactsService);
 }
