b53e387c602a59039ab8735717b4d98c48979020
Aline Dobo <dobo.aline@gmail.com>
2026-08-26 10:59:41 +0200

    Fix: Regex expression for contact name in creation form (again)

--- src/app/components/contacts-comp/contacts-form/contacts-form.ts
+++ src/app/components/contacts-comp/contacts-form/contacts-form.ts
@@ -36,9 +36,9 @@ export class ContactsForm {
     // Reactive form with validators
     contactForm = new FormGroup({
         name: new FormControl('', {
-            /* validators: [Validators.required, Validators.pattern(/^(\w+\s+\w+)/)], */
-            /* validators: [Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i)], */
-            validators: [Validators.required, Validators.pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u)],
+            /* validators: [Validators.required, Validators.pattern(/^(\w+\s+\w+)/)], */ // only checks for 2 words
+            /* validators: [Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i)], */ // only checks for valid characters, not 2 words
+            validators: [Validators.required, Validators.pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+\s+[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/i)],
             // https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
         }),
 
