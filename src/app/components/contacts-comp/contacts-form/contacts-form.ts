4ba6d465f10a9106a7597c34c26846a8d8e59933
Aline Dobo <dobo.aline@gmail.com>
2026-08-26 12:00:59 +0200

    Fix: regex

--- src/app/components/contacts-comp/contacts-form/contacts-form.ts
+++ src/app/components/contacts-comp/contacts-form/contacts-form.ts
@@ -38,7 +38,7 @@ export class ContactsForm {
         name: new FormControl('', {
             /* validators: [Validators.required, Validators.pattern(/^(\w+\s+\w+)/)], */ // only checks for 2 words
             /* validators: [Validators.required, Validators.pattern(/^[a-z ,.'-]+$/i)], */ // only checks for valid characters, not 2 words
-            validators: [Validators.required, Validators.pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+\s+[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/i)],
+            validators: [Validators.required, Validators.pattern(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð .'-]+\s+[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžæÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð .'-]+$/i)],
             // https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
         }),
 
