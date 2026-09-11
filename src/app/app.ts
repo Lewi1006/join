5814123b319b51f64441092ed030cd9ce58d7b9f
Lewi1006 <coach.wind11@gmail.com>
2026-09-11 11:48:02 +0200

    feat: routing set up for login with nested routes/children

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './layout/header/header';
-import { Navigation } from './layout/navigation/navigation';
 import { AlertComp } from "./shared/components/alert-comp/alert-comp";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Navigation, AlertComp],
+  imports: [RouterOutlet, AlertComp],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
