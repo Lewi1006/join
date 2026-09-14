d119aa6878535a2cb2ae5a6a61c1af866a1a25ce
Lewi1006 <coach.wind11@gmail.com>
2026-09-14 22:55:21 +0200

    feat: login form group

--- src/app/components/login/login-comp/login-comp.ts
+++ src/app/components/login/login-comp/login-comp.ts
@@ -1,18 +1,38 @@
 import { Component, inject } from '@angular/core';
+import { FormGroup, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-login-comp',
-  imports: [],
-  templateUrl: './login-comp.html',
-  styleUrl: './login-comp.scss',
+    selector: 'app-login-comp',
+    imports: [ReactiveFormsModule],
+    templateUrl: './login-comp.html',
+    styleUrl: './login-comp.scss',
 })
 export class LoginComp {
-router = inject(Router)
+    router = inject(Router);
 
-goToSignUp(){
-  this.router.navigate(['/signup']);
-}
+    goToSignUp() {
+        this.router.navigate(['/signup']);
+    }
 
+    login(){
+      console.log('login clicked');
+    }
 
+    loginForm = new FormGroup({
+        email: new FormControl('', {
+            validators: [
+                Validators.required,
+                Validators.pattern(
+                    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                ),
+            ],
+        }),
+        password: new FormControl('', {
+            validators: [
+                Validators.required,
+                Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/),
+            ],
+        }),
+    });
 }
