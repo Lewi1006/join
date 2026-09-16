0f518613d835bf329faf5ef17044f5212fffab3e
Lewi1006 <coach.wind11@gmail.com>
2026-09-16 21:02:29 +0200

    feat: nav for galaxy9 styling responsive

--- src/app/shared/auth.guard.ts
+++ src/app/shared/auth.guard.ts
@@ -17,8 +17,8 @@ export class AuthGuard implements CanActivate {
         if (this.authService.isLoggedIn()) {
             return true;
         } else{
-            return this.router.createUrlTree(['/login']);
-            // return true;
+            // return this.router.createUrlTree(['/login']);
+            return true;
         }
     }
 }
