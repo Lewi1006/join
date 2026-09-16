69b367d3c12790dc84ced7d5217523a345992bea
Aline Dobo <dobo.aline@gmail.com>
2026-09-16 20:26:56 +0200

    styling: welcome message

--- src/app/components/summary-comp/summary-comp.ts
+++ src/app/components/summary-comp/summary-comp.ts
@@ -15,8 +15,6 @@ import { Contact } from '../../shared/interfaces/contact.interface';
 export class SummaryComp {
     taskService = inject(TasksService);
     authService = inject(AuthService);
-
-    //#regionstart
     status = {
         todo: TaskStatus.Todo,
         inProgress: TaskStatus.InProgress,
@@ -26,11 +24,31 @@ export class SummaryComp {
     dueDate = '';
     dueDateUpcoming = signal(true);
     private interval: any;
-//#endregion
 
-currentUserName = '';
+    totalNumberOfTasks = computed(() => {
+        const totalNumberOfTasks: number = this.taskService.tasks().length;
+        return totalNumberOfTasks;
+    });
 
-    currentUser = signal<Contact | undefined>(undefined);
+    urgentUndoneTasks = computed(() => {
+        const numberOfUndoneTasksInPriority: number = this.taskService
+            .tasks()
+            .filter((t) => t.priority === 'Urgent' && t.status != this.status.done).length;
+        return numberOfUndoneTasksInPriority;
+    });
+
+    displayedDueDate = computed(() => {
+        const dueDates = this.taskService
+            .tasks()
+            .filter((t) => t.status != this.status.done)
+            .filter((t) => t.dueDate)
+            .map((t) => t.dueDate!)
+            .sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
+
+        const displayedDueDate = dueDates[0];
+
+        return displayedDueDate ?? null;
+    });
 
     ngOnInit() {
         this.taskService.getAllTasks();
@@ -38,7 +56,6 @@ currentUserName = '';
             this.getDueDate();
         }, 50);
     }
-
 
     ngOnDestroy() {
         clearInterval(this.interval);
@@ -67,35 +84,16 @@ currentUserName = '';
         }
     }
 
-    totalNumberOfTasks = computed(() => {
-        const totalNumberOfTasks: number = this.taskService.tasks().length;
-        return totalNumberOfTasks;
-    });
-
-    urgentUndoneTasks = computed(() => {
-        const numberOfUndoneTasksInPriority: number = this.taskService
-            .tasks()
-            .filter((t) => t.priority === 'Urgent' && t.status != this.status.done).length;
-        return numberOfUndoneTasksInPriority;
-    });
-
-    displayedDueDate = computed(() => {
-        const dueDates = this.taskService
-            .tasks()
-            .filter((t) => t.status != this.status.done)
-            .filter((t) => t.dueDate)
-            .map((t) => t.dueDate!)
-            .sort((a, b) => new Date(a).getTime() - new Date(b).getTime());
-
-        const displayedDueDate = dueDates[0];
-
-        return displayedDueDate ?? null;
-    });
-
     tasksByStatus(status: TaskStatus) {
         let numberOfTasksInStatus: number = this.taskService
             .tasks()
             .filter((t) => t.status === status).length;
         return numberOfTasksInStatus;
+    }
+
+    getTimeOfDay() {
+        const date: Date = new Date();
+        const hours: number = date.getHours();
+        return hours;
     }
 }
